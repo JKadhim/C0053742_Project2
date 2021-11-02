9196e21b39c211bdf2390209bd1e116477e2b541
joshua <c0053742@newcastle.ac.uk>
2021-11-02 00:31:11 +0000

    Added a method to read all of the contacts and print them in a neat format

--- contacts.py
+++ contacts.py
@@ -2,5 +2,22 @@ class Contacts:
     def ContactsAdd(self, name, address, phone, age):
         f = open("Book.txt", "a")
         l = [name, address, phone, age]
-        f.write(name + ", " + address + ", " + phone + ", " + age + "\n")
+        for elements in l:
+            f.write(elements + "\n")
+
+
+    def ContactsList(self):
+        f = open("Book.txt","r")
+        lineCount=0
+        for line in f:
+            if line !="\n":
+                lineCount += 1
+        lineGroup = int(lineCount/4)
         f.close()
+        with open("Book.txt","r") as file:
+            readline=file.read().split("\n")
+            i = 0
+            for x in range(0, lineGroup):
+                print ('[{}, {}, {}, {}]'.format(readline[i], readline[i+1],
+                                                  readline[i+2], readline[i+3]))
+                i=i+4
